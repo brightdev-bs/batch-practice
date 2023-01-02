6c85b5e9009f26860c1f72d14704fb134ab063e4
Byeongsu KIM <brightdev.bs@gmail.com>
2023-01-03 07:10:36 +0900

    Batch, JPA config

---
+++ src/main/java/vanille/batch/config/BatchConfig.java
@@ -0,0 +1,10 @@
+package vanille.batch.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+
+@EnableBatchProcessing
+@Configuration
+public class BatchConfig {
+}

1c917eb345ec90a435619842b2282bb7b0fb9ac2
Byeongsu KIM <brightdev.bs@gmail.com>
2023-01-04 11:51:25 +0900

    Notification 엔티티 추가

--- src/main/java/vanille/batch/repository/booking/BookingEntity.java
+++ src/main/java/vanille/batch/repository/booking/BookingEntity.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import vanille.batch.BaseEntity;
+import vanille.batch.repository.pass.PassEntity;
+import vanille.batch.repository.user.UserEntity;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -28,5 +30,19 @@ public class BookingEntity extends BaseEntity {
     private LocalDateTime startedAt;
     private LocalDateTime endedAt;
     private LocalDateTime cancelledAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", insertable = false, updatable = false)
+    private UserEntity userEntity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "passSeq", insertable = false, updatable = false)
+    private PassEntity passEntity;
+
+    // endedAt 기준, yyyy-MM-HH 00:00:00
+    public LocalDateTime getStatisticsAt() {
+        return this.endedAt.withHour(0).withMinute(0).withSecond(0).withNano(0);
+
+    }
 
 }
---
+++ src/main/java/vanille/batch/repository/notification/NotificationEvent.java
@@ -0,0 +1,5 @@
+package vanille.batch.repository.notification;
+
+public enum NotificationEvent {
+    BEFORE_CLASS
+}
